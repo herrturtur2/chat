fa40db91d9d0b73a26e301c29b99606f2e36b291
Joshua Marshall Moore <joshua.marshall.moore@gmail.com>
2010-07-25 17:52:27 -0700

    added register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+// 1. Creates all the necessary tables if they do not yet exist.
+// 2. Adds the user to the database table.
+
+
+// enable error reporting
+error_reporting(E_ALL);
+ini_set("error_reporting", 1);
+
+
+// lots of stuff todo
+// open the database
+// create a user table (id int primary key, name text)
+// insert $_POST['username'] into the user table
+// (return the id of username to the client)
+
+?>
---
+++ scripts.js
@@ -0,0 +1,4 @@
+function usernameOnKeyDown(){
+    var usernameEntry = document.getElementById('username');
+    var username = 
+}
