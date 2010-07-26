50f6c9b33afbd23f2a88b5052d754510ef028e7f
Joshua Marshall Moore <joshua.marshall.moore@gmail.com>
2010-07-25 18:29:50 -0700

    added unregister.php and getOnlineUsers.php

---
+++ getOnlineUsers.php
@@ -0,0 +1,13 @@
+<?php
+
+// turn on error reporting
+error_reporting(E_ALL);
+set_ini("error_reporting", true);
+
+// open database 
+
+// select all online users from table user
+
+// construct json string containing an array of all users
+
+?>
---
+++ unregister.php
@@ -0,0 +1,10 @@
+<?php
+
+// turn on error reporting
+error_reporting(E_ALL);
+set_ini("error_reporting", 1);
+
+// open database
+// remove $_POST['username'] from table user
+
+?>
